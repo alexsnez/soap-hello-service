d14cb8511dde4e23e4e9495437dfed4be6184613
Aleksandr <alexnezdemina@gmail.com>
2025-10-15 23:02:38 +0300

    Create HelloRequest.java

---
+++ src/main/java/com/example/soap/model/HelloRequest.java
@@ -0,0 +1,14 @@
+package com.example.soap.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "HelloRequest", namespace = "http://learnwebservices.com/services/hello")
+public class HelloRequest {
+    private String name;
+
+    @XmlElement(name = "Name")
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+}
