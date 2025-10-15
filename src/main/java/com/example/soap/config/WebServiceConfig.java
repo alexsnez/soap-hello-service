6de52860b0dec9682f4eb712f2b424bac1b34e2d
Aleksandr <alexnezdemina@gmail.com>
2025-10-15 23:04:46 +0300

    Add WebServiceConfig for SOAP web service

---
+++ src/main/java/com/example/soap/config/WebServiceConfig.java
@@ -0,0 +1,36 @@
+package com.example.soap.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.Bean;
+import org.springframework.context.Configuration;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@Configuration
+public class WebServiceConfig {
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "hello")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema helloSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("HelloPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://learnwebservices.com/services/hello");
+        wsdl11Definition.setSchema(helloSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema helloSchema() {
+        return new SimpleXsdSchema(new org.springframework.core.io.ClassPathResource("wsdl/hello.wsdl"));
+    }
+}
