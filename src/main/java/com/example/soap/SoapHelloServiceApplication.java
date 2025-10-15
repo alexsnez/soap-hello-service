01b3d5ed4fde3d427f36f18665277f4200062b53
Aleksandr <alexnezdemina@gmail.com>
2025-10-15 23:45:39 +0300

    Create SoapHelloServiceApplication.java

---
+++ src/main/java/com/example/soap/SoapHelloServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapHelloServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoapHelloServiceApplication.class, args);
+    }
+}
