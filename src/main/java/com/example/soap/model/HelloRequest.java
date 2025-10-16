5b7f00a92717cf96d90e3a80f5dd504b4bdeb52f
Aleksandr <alexnezdemina@gmail.com>
2025-10-16 11:12:34 +0300

    Update HelloRequest.java

--- src/main/java/com/example/soap/model/HelloRequest.java
+++ src/main/java/com/example/soap/model/HelloRequest.java
@@ -1,14 +1,22 @@
 package com.example.soap.model;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "HelloRequest", namespace = "http://learnwebservices.com/services/hello")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class HelloRequest {
+
+    @XmlElement(name = "Name", namespace = "http://learnwebservices.com/services/hello", required = true)
     private String name;
 
-    @XmlElement(name = "Name")
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
